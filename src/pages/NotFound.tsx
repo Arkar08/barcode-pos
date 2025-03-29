e39d459dee9e43b31dbaf47ead61449f6a6183d9
uiux@okdollar frontend <uiux@okdollar.com>
2025-03-29 11:10:21 +0630

    notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,50 @@
+import { Button } from "antd"
+import { useNavigate } from "react-router-dom"
 
+const notFoundContainer:React.CSSProperties = {
+  display:'flex',
+  justifyContent:"center",
+  alignItems:'center',
+  height:'100vh',
+  flexDirection:"column"
+}
+
+const notFoundText:React.CSSProperties = {
+  fontSize:48,
+  letterSpacing:3,
+  fontWeight:500
+}
+
+const backButton:React.CSSProperties = {
+  width:'230px',
+  marginTop:30,
+  fontSize:18,
+  padding:10,
+  height:50,
+  boxShadow:'1px 1px 3px black',
+  border:'none'
+}
+
+const backArrow:React.CSSProperties = {
+  width:'30px',
+  height:'30px'
+}
 
 const NotFound = () => {
+
+  const navigate = useNavigate()
+
+  const backToHome = () =>{
+    navigate("/dashboard")
+  }
+
   return (
-    <div>
-      NotFound
+    <div style={notFoundContainer}>
+      <h3 style={notFoundText}>404 Not Found Page</h3>
+      <Button variant="outlined" style={backButton} onClick={backToHome}>
+        <img src="/images/left-arrow.png" alt="back_arrow"  style={backArrow}/>
+        <span>Back To Dashboard</span>
+      </Button>
     </div>
   )
 }
